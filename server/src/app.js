21dceeb6af1481e554a2700ee9e783e144b1ee1e
brittneypostma <brittney.postma@hotmail.com>
2021-05-12 17:15:12 -0400

    added cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const cors = require('cors')
+
 const itemsRouter = require('./routes/items/items.router')
 
 const app = express();
 
+app.use(cors({
+  // use port of frontend dev server
+  origin: 'http://localhost:3000'
+}))
 app.use(express.json())
 app.use(itemsRouter)
 
