1f5d97be054e5f2e61234a3ec32abd8557bf5bb3
brittneypostma <brittney.postma@hotmail.com>
2021-06-05 17:46:57 -0400

    added packages, syncing with course

--- server/src/models/cart.js
+++ server/src/models/cart.js
@@ -3,27 +3,29 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const CartSchema = new Schema({
-  userId: {
-      type: String,
-  },
-  items: [{
-      productId: {
-          type: String,
-      },
-      name: String,
-      quantity: {
-          type: Number,
-          required: true,
-          min: [1, 'Quantity can not be less then 1.'],
-          deafult: 1
-      },
-      price: Number
-  }],
-  total: {
-      type: Number,
-      required: true,
-      default: 0
-  }
-});
+	userId: {
+		type: String
+	},
+	items: [
+		{
+			productId: {
+				type: String
+			},
+			name: String,
+			quantity: {
+				type: Number,
+				required: true,
+				min: [1, 'Quantity can not be less then 1.'],
+				default: 1
+			},
+			price: Number
+		}
+	],
+	total: {
+		type: Number,
+		required: true,
+		default: 0
+	}
+})
 
-module.exports =  mongoose.model('Cart', CartSchema);
+module.exports = mongoose.model('Cart', CartSchema)
