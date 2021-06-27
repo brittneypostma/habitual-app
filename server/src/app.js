75411c8420e1d3770f015e4ac867217963e27bad
brittneypostma <brittney.postma@hotmail.com>
2021-06-27 16:43:51 -0400

    fixed server

--- server/src/app.js
+++ server/src/app.js
@@ -13,16 +13,16 @@ app.use(helmet())
 app.use(
   cors({
     // use port of frontend dev server
-    origin: 'http://localhost:8080',
+    origin: 'http://localhost:3000',
   })
 )
 app.use(morgan('combined'))
 app.use(express.json())
-app.use(express.static(path.join(__dirname, '..', 'build')))
+app.use(express.static(path.join(__dirname, '../client/src')))
 app.use(itemsRouter)
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
+  res.sendFile(path.join(__dirname, '../../client/src', 'index.html'))
 })
 
 module.exports = app
--- server/src/server.js
+++ server/src/server.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const app = require('./app')
 const { mongoConnect } = require('./services/mongo')
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 3000
 
 const server = http.createServer(app)
 
