7f051172ec690fa0eb950b294280c6717b652226
brittneypostma <brittney.postma@hotmail.com>
2021-06-28 10:58:44 -0400

    ubuntu

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const path = require('path')
 const morgan = require('morgan')
 const helmet = require('helmet')
-const {auth} = require('express-openid-connect')
+const { auth } = require('express-openid-connect')
 const itemsRouter = require('./routes/item/item.router')
 
 const app = express()
@@ -19,19 +19,25 @@ app.use(
 app.use(morgan('combined'))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '../client/src')))
-app.use(auth({
-  authRequired: false,
-  auth0Logout: true,
-  issuerBaseURL: process.env.AUTH0_DOMAIN,
-  baseURL: process.env.BASE_URL,
-  clientID: process.env.AUTH0_CLIENT_ID,
-  secret: process.env.SESSION_SECRET
-}))
+app.use(
+  auth({
+    authRequired: false,
+    auth0Logout: true,
+    issuerBaseURL: process.env.AUTH0_DOMAIN,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    secret: process.env.SESSION_SECRET,
+  })
+)
 
 app.use(itemsRouter)
 
 app.get('/*', (req, res) => {
-  res.sendFile(req.oidc.isAuthenticated() ? (path.join(__dirname, '../../client/src', 'index.html')) : (path.join(__dirname, '../../client/src', 'home.html')))
+  res.sendFile(
+    req.oidc.isAuthenticated()
+      ? path.join(__dirname, '../../client/src', 'home.html')
+      : path.join(__dirname, '../../client/src', 'index.html')
+  )
 })
 
 module.exports = app
