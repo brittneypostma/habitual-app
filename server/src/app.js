74ea866b2b61dc9bd56a96b2af8784e44bdd3191
brittneypostma <brittney.postma@hotmail.com>
2021-06-29 20:24:01 -0400

    working on new auth system

--- server/src/app.js
+++ server/src/app.js
@@ -3,9 +3,9 @@ const express = require('express')
 const path = require('path')
 const morgan = require('morgan')
 const helmet = require('helmet')
-const { auth } = require('express-openid-connect')
-const itemsRouter = require('./routes/item/item.router')
-
+// const { auth } = require('express-openid-connect')
+// const itemsRouter = require('./routes/item/item.router')
+const routes = require('./routes/index')
 const app = express()
 
 app.use(helmet())
@@ -16,28 +16,39 @@ app.use(
     origin: 'http://localhost:3000',
   })
 )
-app.use(morgan('combined'))
-app.use(express.json())
-app.use(express.static(path.join(__dirname, '../client/src')))
-app.use(
-  auth({
-    authRequired: false,
-    auth0Logout: true,
-    issuerBaseURL: process.env.AUTH0_DOMAIN,
-    baseURL: process.env.BASE_URL,
-    clientID: process.env.AUTH0_CLIENT_ID,
-    secret: process.env.SESSION_SECRET,
-  })
-)
 
-app.use(itemsRouter)
+//* Logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan(dev))
+} else app.use(morgan('combined'))
 
-app.get('/*', (req, res) => {
-  res.sendFile(
-    req.oidc.isAuthenticated()
-      ? path.join(__dirname, '../../client/src', 'home.html')
-      : path.join(__dirname, '../../client/src', 'index.html')
-  )
-})
+//* Static Folder
+app.use(express.static(path.join(__dirname, '../../public')))
+
+//* Routes
+app.use('/', routes)
+
+// app.use(express.json())
+// app.use(express.static(path.join(__dirname, '../client/src')))
+// app.use(
+//   auth({
+//     authRequired: false,
+//     auth0Logout: true,
+//     issuerBaseURL: process.env.AUTH0_DOMAIN,
+//     baseURL: process.env.BASE_URL,
+//     clientID: process.env.AUTH0_CLIENT_ID,
+//     secret: process.env.SESSION_SECRET,
+//   })
+// )
+
+// app.use(itemsRouter)
+
+// app.get('/*', (req, res) => {
+//   res.sendFile(
+//     req.oidc.isAuthenticated()
+//       ? path.join(__dirname, '../../client/src', 'home.html')
+//       : path.join(__dirname, '../../client/src', 'index.html')
+//   )
+// })
 
 module.exports = app
--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,22 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-const MONGO_URL = process.env.MONGO_URL
+const MONGO_URI = process.env.MONGO_URI
+
+// const mongoConnect = async () => {
+//   try {
+//     const connection = await mongoose.connect(MONGO_URI, {
+//       useNewUrlParser: true,
+//       useFindAndModify: false,
+//       useCreateIndex: true,
+//       useUnifiedTopology: true,
+//     })
+//     console.log(`MongoDB connected: ${connection.connection.host}`)
+//   } catch (e) {
+//     console.error(e)
+//     process.exit(1)
+//   }
+// }
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!')
@@ -12,7 +27,7 @@ mongoose.connection.on('error', (e) => {
 })
 
 async function mongoConnect() {
-  await mongoose.connect(MONGO_URL, {
+  await mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true,
