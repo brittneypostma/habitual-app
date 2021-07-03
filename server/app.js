7aa1a7eef8a6256eeb428f3f1a4c79cc420013bb
brittneypostma <brittney.postma@hotmail.com>
2021-07-03 15:27:12 -0400

    esbuild

--- server/app.js
+++ server/app.js
@@ -43,7 +43,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 //* Static Folder
-app.use(express.static(path.join(__dirname, '../../public')))
+app.use(express.static(path.join(__dirname, '../public')))
 
 //* Routes
 app.use('/auth', authRoutes)
